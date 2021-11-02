212062f0364c180a14b9b7db8b8371bec77ffc0f
mona M <monam@monas-MacBook-Pro.local>
2021-11-02 14:53:11 +0300

    Completed SwiftFundamentals-7

---
+++ PropertyObsarver-MonaAlqahtani.playground/Pages/01-01 Person.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+
+import Foundation
+
+
+struct Person {
+  
+  var clothe: String {
+    willSet(newClothe){
+      print("I'm changing from \(clothe) to \(newClothe)")
+    }
+    didSet(oldClothe){
+      print("I've just changed from \(oldClothe) to \(clothe)")
+    }
+  }
+}
+
+var person = Person (clothe:"Blue T-shirt")
+person.clothe = "white T-shirt"
